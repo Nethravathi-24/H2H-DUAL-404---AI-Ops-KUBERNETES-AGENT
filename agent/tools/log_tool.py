62da8d97cf98197e291174e453111e80ed128748
Nethravathi-24 <nethravathid490@gmail.com>
2026-04-19 13:55:30 +0530

    Updated  log_tool.py

--- agent/tools/log_tool.py
+++ agent/tools/log_tool.py
@@ -1,18 +1,37 @@
 import subprocess
 
 
-def get_pod_logs(pod_name):
+def get_pod_logs(pod_name=None):
     """
-    Fetch logs from a pod
+    Get logs from a specific pod
     """
+
     try:
-        result = subprocess.run(
-            ["kubectl", "logs", pod_name],
+        # If pod name not provided, get first pod
+        if pod_name is None:
+            pod_cmd = ["kubectl", "get", "pods"]
+
+            pod_result = subprocess.run(
+                pod_cmd,
+                capture_output=True,
+                text=True
+            )
+
+            lines = pod_result.stdout.split("\n")
+
+            if len(lines) > 1:
+                pod_name = lines[1].split()[0]
+
+        # Get logs
+        log_cmd = ["kubectl", "logs", pod_name]
+
+        log_result = subprocess.run(
+            log_cmd,
             capture_output=True,
             text=True
         )
 
-        return result.stdout
+        return log_result.stdout
 
     except Exception as e:
         return str(e)
